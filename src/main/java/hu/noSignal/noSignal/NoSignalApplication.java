0dc5ba181de0a44ad561ca008c203c219c78bcbb
Vadász Péter <pevad95@gmail.com>
2017-10-30 12:28:12 +0100

    noSignal-1: Initialize spring.

---
+++ src/main/java/hu/noSignal/noSignal/NoSignalApplication.java
@@ -0,0 +1,12 @@
+package hu.noSignal.noSignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSignalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoSignalApplication.class, args);
+	}
+}
